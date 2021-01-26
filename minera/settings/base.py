3c1fabc7e45ac5f971fdd1f4d7e9b4e8b457684d
Alexander Calani <alexander.calani.apaza@gmail.com>
2021-01-26 17:37:26 -0400

    investigando archivos estaticos segunda version

--- minera/settings/base.py
+++ minera/settings/base.py
@@ -13,7 +13,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(
+    os.path.abspath(os.path.join(__file__, os.pardir))))
 
 
 # Quick-start development settings - unsuitable for production
