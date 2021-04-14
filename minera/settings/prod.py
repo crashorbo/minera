365902d8edec2d2a1888fb9c9b9190e6bcfe9746
Alexander Calani <alexander.calani.apaza@gmail.com>
2021-04-13 20:28:24 -0400

    configuracion bd prod y requirements"

--- minera/settings/prod.py
+++ minera/settings/prod.py
@@ -10,10 +10,10 @@ ADMINS = (
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'd13vn99tm5dsb2',
-        'USER': 'qrabwjwrgvikmm',
-        'PASSWORD': 'bc23325b1f12992df718faf8ffb9d49a2fefb6191d6b183cae58052484891a7d',
-        'HOST': 'ec2-54-211-55-24.compute-1.amazonaws.com',
+        'NAME': 'emcoisa',
+        'USER': 'alexander',
+        'PASSWORD': 'add150806',
+        'HOST': 'localhost',
         'PORT': 5432,
     }
 }
