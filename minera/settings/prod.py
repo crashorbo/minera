0bd46a4e9cb59ecc38c016916c7f9d81c7f18aa9
Alexander Calani <alexander.calani.apaza@gmail.com>
2021-06-22 17:27:48 -0400

    cambios en la seccion de contabilidad

--- minera/settings/prod.py
+++ minera/settings/prod.py
@@ -11,7 +11,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         # 'NAME': 'emcoisa',
-        'NAME': 'minera_prod',
+        'NAME': 'minera_dev',
         'USER': 'alexander',
         'PASSWORD': 'add150806',
         'HOST': 'localhost',
