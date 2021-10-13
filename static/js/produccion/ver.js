77b929e89dd6f8e8b3b9921f3c9c888f14b74135
Alexander Calani <alexander.calani.apaza@gmail.com>
2021-10-12 23:48:32 -0400

    porevia senalizacion en produccion

--- static/js/produccion/ver.js
+++ static/js/produccion/ver.js
@@ -3,3 +3,26 @@ const loader = document.querySelector('.loader');
 window.addEventListener('DOMContentLoaded', () => {
     loader.style.visibility = 'hidden';
 });
+
+const listaCargas = document.querySelector("#lista-cargas");
+console.log(listaCargas);
+listaCargas.addEventListener('click', (e) => {
+    if (e.target.classList.contains("bi-bullseye")){
+        console.log("prueba")
+        e.target.classList.remove("bi-bullseye");
+        e.target.classList.add("bi-check-circle-fill");
+        return ;
+    }
+
+    if (e.target.classList.contains("bi-check-circle-fill")){
+        e.target.classList.remove("bi-check-circle-fill");
+        e.target.classList.add("bi-bullseye");
+        return;
+    }
+
+    if (e.target.classList.contains("bi-trash")){
+        console.log(e.target.closest(".row-carga"));
+        return;
+    }
+
+});
