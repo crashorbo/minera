fbeb133d2e295d1a285b993dc5bcdb2adb42e091
Alexander Calani <alexander.calani.apaza@gmail.com>
2021-12-16 17:28:45 -0400

    busqueda de bolsa en admin

--- user/admin.py
+++ user/admin.py
@@ -1,5 +1,12 @@
 from user.models import CustomUser
 from django.contrib import admin
 
+from .models import Codigo
 # Register your models here.
 admin.site.register(CustomUser)
+
+class CodigoAdmin(admin.ModelAdmin):
+    list_display=('numero', 'cod_externo', 'cod_proveedor', 'cod_testigo', 'cod_bolsa')
+    search_fields = ['numero',]    
+    
+admin.site.register(Codigo, CodigoAdmin)
