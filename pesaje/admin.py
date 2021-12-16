e855f95cdc5353a83b4095b252a0b0720d7c5eba
Alexander Calani <alexander.calani.apaza@gmail.com>
2021-12-16 17:11:37 -0400

    busqueda de item en muestra

--- pesaje/admin.py
+++ pesaje/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from pesaje.models import Muestra
 
 # Register your models here.
-admin.site.register(Muestra)
+class MuestraAdmin(admin.ModelAdmin):
+    search_fields = ['codigo',]
+    
+admin.site.register(Muestra, MuestraAdmin)
